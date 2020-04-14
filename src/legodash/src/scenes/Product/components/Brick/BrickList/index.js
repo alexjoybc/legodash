36424d5a70558f9ec6729a8b7e514c672e34a48b
alex <alexandre.joyeux@nttdata.com>
2020-04-13 21:35:27 -0700

    Reorganize structure

--- src/legodash/src/scenes/Product/components/Brick/BrickList/index.js
+++ src/legodash/src/scenes/Product/components/Brick/BrickList/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup"
-import BrickItem from "./components/BrickItem"
+import BrickItem from "../BrickItem"
 
 
 export default function BrickList({ bricks }) {
