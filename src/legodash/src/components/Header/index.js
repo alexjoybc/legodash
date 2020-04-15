24172674d495269a0ddc6411199bfbf8d96c2046
alex <alexandre.joyeux@nttdata.com>
2020-04-14 22:18:49 -0700

    add search bar

--- src/legodash/src/components/Header/index.js
+++ src/legodash/src/components/Header/index.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, InputGroup, FormControl, Button } from "react-bootstrap";
 
 export default function Header() {
   return (
     <Navbar bg="light" expand="lg">
-  <Link className="navbar-brand" to="/">Legodash</Link>
-  
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/about">About</Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+      <Link className="navbar-brand" to="/">
+        Legodash
+      </Link>
+      <InputGroup>
+        <FormControl
+          placeholder="Search by product code"
+          aria-label="Search by product code"
+          aria-describedby="Search by product code"
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Search</Button>
+        </InputGroup.Append>
+      </InputGroup>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
